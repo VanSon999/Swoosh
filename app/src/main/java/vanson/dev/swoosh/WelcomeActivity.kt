1aeadfa955e4e32321400a93a75808af38ea577f
VanSon999 <nguyenvanson95999@gmail.com>
2020-12-14 14:42:31 +0700

    switch activity

--- app/src/main/java/vanson/dev/swoosh/WelcomeActivity.kt
+++ app/src/main/java/vanson/dev/swoosh/WelcomeActivity.kt
@@ -1,11 +1,17 @@
 package vanson.dev.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
