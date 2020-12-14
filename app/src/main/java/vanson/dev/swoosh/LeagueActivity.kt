39a8264cc52be8d0fbbc1143422d6f8e6debfa27
VanSon999 <nguyenvanson95999@gmail.com>
2020-12-14 16:11:36 +0700

    use intent to send data

--- app/src/main/java/vanson/dev/swoosh/LeagueActivity.kt
+++ app/src/main/java/vanson/dev/swoosh/LeagueActivity.kt
@@ -4,14 +4,53 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+    var selectLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
     fun leagueNextClicked(view: View){
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if(selectLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectLeague)
+            startActivity(skillActivity)
+        }else{
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun onMensClicked(view: View){
+        wonmensBtn.isChecked = false
+        coBtn.isChecked = false
+        if(mensBtn.isChecked){
+            selectLeague = "mens"
+        }else{
+            selectLeague = ""
+        }
+    }
+
+    fun onWomensClicked(view: View){
+        mensBtn.isChecked = false;
+        coBtn.isChecked = false;
+//        selectLeague = "womens"
+        if(wonmensBtn.isChecked){
+            selectLeague = "womens"
+        }else{
+            selectLeague = ""
+        }
+    }
+
+    fun onCoClicked(view: View){
+        mensBtn.isChecked = false;
+        wonmensBtn.isChecked = false;
+//        selectLeague = "co-ed"
+        if(coBtn.isChecked){
+            selectLeague = "co-ed"
+        }else{
+            selectLeague = ""
+        }
     }
 }
