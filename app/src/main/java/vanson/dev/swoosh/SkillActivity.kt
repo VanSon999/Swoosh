54abeca0210b099847b73a85159ac5ce1f5e56b0
VanSon999 <nguyenvanson95999@gmail.com>
2020-12-14 15:45:30 +0700

    Skill Activity

---
+++ app/src/main/java/vanson/dev/swoosh/SkillActivity.kt
@@ -0,0 +1,11 @@
+package vanson.dev.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
--- app/src/main/java/vanson/dev/swoosh/WelcomeActivity.kt
+++ app/src/main/java/vanson/dev/swoosh/WelcomeActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
